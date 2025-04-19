751843c2f41ff0a27496e1525adc6a92d34c7901
Sheared1 <lucaswilliamstech@outlook.com>
2025-04-19 13:27:20 -0400

    Added User entity, references user table and user_roles table.

---
+++ src/main/java/com/urlshortener/demo/User/User.java
@@ -0,0 +1,97 @@
+package com.urlshortener.demo.User;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean active = true;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "last_login_at")
+    private LocalDateTime lastLoginAt;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id")     //Finding roles by user id, one user can have many roles.
+    )
+    @Column(name = "role")
+    private Set<String> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getLastLoginAt() {
+        return lastLoginAt;
+    }
+
+    public void setLastLoginAt(LocalDateTime lastLoginAt) {
+        this.lastLoginAt = lastLoginAt;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+
+    // Getters and setters
+
+    public boolean isActive() {
+        return active;
+    }
+
+}
---
+++ src/main/java/com/urlshortener/demo/User/UserService.java
@@ -0,0 +1,7 @@
+package com.urlshortener.demo.User;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
