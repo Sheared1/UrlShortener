587e42eee873afad64e41413fac43c45c6ddd29e
Sheared1 <lucaswilliamstech@outlook.com>
2025-05-31 07:00:28 -0400

    Update CORS allowed origin to new domain

--- src/main/java/com/urlshortener/demo/WebConfig/WebConfig.java
+++ src/main/java/com/urlshortener/demo/WebConfig/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://sheared1.github.io/UrlShortener")
+                .allowedOrigins("https://sheared.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
