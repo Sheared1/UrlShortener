11a1818a12093fd17867ea316445829e93fef335
Sheared1 <lucaswilliamstech@outlook.com>
2025-05-31 04:25:34 -0400

    Testing GitHub Pages

---
+++ src/main/java/com/urlshortener/demo/WebConfig/WebConfig.java
@@ -0,0 +1,15 @@
+package com.urlshortener.demo.WebConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://sheared1.github.io")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
