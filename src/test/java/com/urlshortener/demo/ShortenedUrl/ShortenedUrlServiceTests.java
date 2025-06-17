9066cd2d6f4b192f62f39b308fcf6acc161a3e96
Sheared1 <lucaswilliamstech@outlook.com>
2025-06-16 21:20:26 -0400

    Added unit tests for retrieving shortened URLs by short code and ID, including handling of invalid short codes.

--- src/test/java/com/urlshortener/demo/ShortenedUrl/ShortenedUrlServiceTests.java
+++ src/test/java/com/urlshortener/demo/ShortenedUrl/ShortenedUrlServiceTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -21,6 +23,48 @@ public class ShortenedUrlServiceTests {
         assertNotNull(shortenedUrl);
         assertNotNull(shortenedUrl.getShortCode());
         assertEquals(originalUrl, shortenedUrl.getOriginalUrl());
+
+    }
+
+    @Test
+    void getShortenedUrlByShortCode_shouldReturnShortenedUrl() {
+
+        String originalUrl = "https://testing.com";
+        ShortenedUrl shortenedUrl = shortenedUrlService.shortenUrl(originalUrl, null, null, null);
+        String shortCode = shortenedUrl.getShortCode();
+
+        ShortenedUrl retrievedUrl = shortenedUrlService.getShortenedUrlByShortCode(shortCode);
+
+        assertNotNull(retrievedUrl);
+        assertEquals(originalUrl, retrievedUrl.getOriginalUrl());
+        assertEquals(shortCode, retrievedUrl.getShortCode());
+
+    }
+
+    @Test
+    void getShortenedUrlByShortCode_shouldReturnNullForInvalidShortCode() {
+
+        String invalidShortCode = "invalidShortCode";
+
+        ShortenedUrl retrievedUrl = shortenedUrlService.getShortenedUrlByShortCode(invalidShortCode);
+
+        assertNull(retrievedUrl);
+
+    }
+
+    @Test
+    void getShortenedUrlById_shouldReturnShortenedUrl() {
+
+        String originalUrl = "https://testing.com";
+        ShortenedUrl shortenedUrl = shortenedUrlService.shortenUrl(originalUrl, null, null, null);
+        long id = shortenedUrl.getId();
+
+        Optional<ShortenedUrl> optionalUrl = shortenedUrlService.getShortenedUrlById(id);
+        ShortenedUrl retrievedUrl = optionalUrl.orElse(null);
+
+        assertNotNull(retrievedUrl);
+        assertEquals(originalUrl, retrievedUrl.getOriginalUrl());
+        assertEquals(shortenedUrl.getShortCode(), retrievedUrl.getShortCode());
 
     }
 
